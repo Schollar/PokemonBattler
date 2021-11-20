feaa46fbbe79e332fcc1b6b48f258fc4d5cdb7b3
schollar <cschollar22@gmail.com>
2021-11-20 10:35:53 -0700

    Added enemy pokemon to battle page

--- js/selection.js
+++ js/selection.js
@@ -3,6 +3,29 @@ function remove_selection() {
     Cookies.remove('chosen');
 }
 
+function enemy_selection(enemy) {
+    var enemy_selection = document.createElement('section');
+    enemy_selection.setAttribute('id', 'enemy_container');
+
+    var enemy_name = document.createElement('h1');
+    enemy_name.innerText = enemy.name;
+    enemy_selection.appendChild(enemy_name);
+
+    var enemy_health = document.createElement('p');
+    var enemy_max_health = enemy.maxhealth;
+    var enemy_current_health = enemy.maxhealth;
+    enemy_health.innerText = `Health: ${enemy_current_health} / ${enemy_max_health}`;
+    enemy_selection.appendChild(enemy_health);
+
+    var enemy_image = document.createElement('img');
+    enemy_image.setAttribute('src', enemy.img_src);
+    enemy_selection.appendChild(enemy_image);
+
+    var attack1 = document.createElement('button');
+    attack1.innerText = `${enemy.attack1} Damage Attack`;
+    enemy_selection.appendChild(attack1);
+    page_container.appendChild(enemy_selection);
+}
 
 function selection_card(user_selection) {
     var selected = chosen_one[user_selection];
@@ -12,20 +35,46 @@ function selection_card(user_selection) {
         var chosen_selection = document.getElementById('chosen_selection');
         // Displaying Chosen message
         chosen_selection.innerText = `You have picked ${selected.name} Nice!`;
+        // setting up our health variables
+        var user_pokemon_health = document.createElement('p');
+        var user_current_health = selected.maxhealth;
+        user_pokemon_health.innerText = `Health: ${user_current_health} / ${selected.maxhealth}`
+        chosen_selection.appendChild(user_pokemon_health);
         // Creating and displaying the chosen image
         var pokemon_selected = document.createElement('img');
         pokemon_selected.setAttribute('src', selected.img_src);
         // Appending image to parent
         chosen_selection.appendChild(pokemon_selected);
+
+        var attack1 = document.createElement('button');
+        attack1.innerText = `${selected.attack1} Damage Attack`;
+        chosen_selection.appendChild(attack1);
+
     }
 }
+
+var enemy_pokemon =
+
+{
+    name: 'Squirtle',
+    img_src: "https://images.saymedia-content.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc2MjY3Mzg3MDczNDcxNjc4/pokemon-squirtle-nicknames.jpg",
+    maxhealth: 40,
+    attack1: 5,
+    attack2: 7,
+    attack3: 9
+
+}
+
+
+var page_container = document.getElementById('page_container');
+
 // Setting up an array of objects to use based on selection
 var chosen_one = {
 
     squirtle: {
         name: 'squirtle',
         img_src: "https://images.saymedia-content.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:eco%2Cw_1200/MTc2MjY3Mzg3MDczNDcxNjc4/pokemon-squirtle-nicknames.jpg",
-        health: 40,
+        maxhealth: 40,
         attack1: 5,
         attack2: 7,
         attack3: 9
@@ -34,7 +83,7 @@ var chosen_one = {
     charmander: {
         name: 'charmander',
         img_src: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-blue-version/d/d4/Charmander.gif",
-        health: 40,
+        maxhealth: 40,
         attack1: 5,
         attack2: 7,
         attack3: 9
@@ -42,7 +91,7 @@ var chosen_one = {
     bulbasaur: {
         name: 'bulbasaur',
         img_src: "https://64.media.tumblr.com/172a6e167359e6b6832116ffac691e87/tumblr_inline_p7ja2uo4ZQ1qhvvv4_500.png",
-        health: 40,
+        maxhealth: 40,
         attack1: 5,
         attack2: 7,
         attack3: 9
@@ -50,7 +99,7 @@ var chosen_one = {
     pikachu: {
         name: 'pikachu',
         img_src: "https://secure.img1-fg.wfcdn.com/im/77981853/resize-h755-w755%5Ecompr-r85/8470/84707680/Pokemon+Pikachu+Wall+Decal.jpg",
-        health: 40,
+        maxhealth: 40,
         attack1: 5,
         attack2: 7,
         attack3: 9
@@ -59,3 +108,5 @@ var chosen_one = {
 
 var user_selection = Cookies.get('chosen');
 selection_card(user_selection);
+
+enemy_selection(enemy_pokemon);
